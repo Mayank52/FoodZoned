b90e9b4f266c69322525f7e2d688382cae97ff06
Mayank52 <mayank.aggarwal963@gmail.com>
2021-03-02 17:22:55 +0530

    update

--- public/js/addReview.js
+++ public/js/addReview.js
@@ -1,32 +1,37 @@
 let addReviewBtn = document.querySelector(".add-review button");
 let reviewTextArea = document.querySelector(".new-review");
 let ratingSelectBox = document.querySelector(".new-review-rating select");
+let allLis = document.querySelectorAll(".link");
 
 //add Review
 addReviewBtn.addEventListener("click", async (e) => {
   try {
-    let review = reviewTextArea.value;
-    let rating = ratingSelectBox.value;
+    if (allLis.length < 6) {
+      window.location.href = "/login";
+    } else {
+      let review = reviewTextArea.value;
+      let rating = ratingSelectBox.value;
 
-    //Currently selected plan has a active class
-    let selectedPlan = document.querySelector(".plan-item.active");
-    let planId = selectedPlan.id;
-    console.log(review, rating, planId);
-    let newReviewObj = {
-      planId: planId,
-      review: review,
-      rating: rating,
-    };
+      //Currently selected plan has a active class
+      let selectedPlan = document.querySelector(".plan-item.active");
+      let planId = selectedPlan.id;
+      console.log(review, rating, planId);
+      let newReviewObj = {
+        planId: planId,
+        review: review,
+        rating: rating,
+      };
 
-    console.log(newReviewObj);
-    let reviewObj = await axios.post(
-      "https://foodzoned--app.herokuapp.com/api/review",
-      newReviewObj
-    );
-    console.log(reviewObj);
+      console.log(newReviewObj);
+      let reviewObj = await axios.post(
+        "https://foodzoned--app.herokuapp.com/api/review",
+        newReviewObj
+      );
+      console.log(reviewObj);
 
-    //update new review in UI
-    window.location.reload();
+      //update new review in UI
+      window.location.reload();
+    }
   } catch (error) {
     console.log(error);
   }
